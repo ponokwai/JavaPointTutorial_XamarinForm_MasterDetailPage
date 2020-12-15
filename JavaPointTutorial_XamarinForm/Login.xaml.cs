defdf2242947061da90cc70d27b5c08425c8ff75
Patrick Onokwai <Patrick.Onokwai@wemabank.com>
2020-12-15 09:01:43 +0100

    Add project files.

---
+++ JavaPointTutorial_XamarinForm/Login.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JavaPointTutorial_XamarinForm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_btn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "Login was successful", "OK");
+            new NavigationPage(new Page1());
+        }
+    }
+}
---
+++ JavaPointTutorial_XamarinForm/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JavaPointTutorial_XamarinForm
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //Detail = new NavigationPage(new Login());
+            //IsPresented = true;
+        }
+
+        public void button_clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        public void button_clicked1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+
+        private void login_btn_clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Login());
+            IsPresented = false;
+        }
+    }
+}
